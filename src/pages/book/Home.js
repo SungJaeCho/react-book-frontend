9a18b495f9bce83a85740ef4194f04e9d080b075
sungjaecho <liadus@naver.com>
2022-01-12 09:07:36 +0900

    커밋

--- src/pages/book/Home.js
+++ src/pages/book/Home.js
@@ -1,10 +1,24 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import BookItem from '../../components/BookItem';
 
 const Home = () => {
+  const [books, setBooks] = useState([]);
+
+  // 함수 실행시 최초 한번 실행되는 것 + 상태값이 변경될때마다 실행 아래에 []를 통해 아무것도 의존하지 않는다고 알려줘야 한번만 실행됨
+  useEffect(() => {
+    fetch('http://localhost:8080/book')
+      .then((res) => res.json())
+      .then((res) => {
+        console.log(1, res);
+        setBooks(res);
+      }); //비동기 함수
+  }, []);
+
   return (
     <div>
-      <BookItem />
+      {books.map((book) => (
+        <BookItem key={book.id} book={book} />
+      ))}
     </div>
   );
 };
